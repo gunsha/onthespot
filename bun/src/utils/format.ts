8796babf67c01b31b1e2c93f285900aeb04c642a
gunsha <gonzaller@gmail.com>
2026-09-05 15:43:38 -0300

    remove js

--- bun/src/utils/format.ts
+++
@@ -1,9 +0,0 @@
-import { config } from '../config/index';
-
-export function convListFormat(items: string[]): string {
-  if (!items || items.length === 0) {
-    return '';
-  }
-  const separator = config.get<string>('metadata_separator') || '; ';
-  return items.join(separator);
-}
